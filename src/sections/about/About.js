d47c290bcb667231622e5979e67ffb3f8af09e4d
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-06-29 11:20:30 -0700

    Added card title and content into section

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -13,18 +13,26 @@ const About = () => {
               <Card />
             </div>
             <div className="column is-half">
-            <div className="tags">
-              <span className="tag is-large is-primary">HTML</span>
-              <span className="tag is-large is-primary">CSS</span>
-              <span className="tag is-large is-primary">JavaScript</span>
-              <span className="tag is-large is-primary">React</span>
-              <span className="tag is-large is-primary">SASS</span>
-              <span className="tag is-large is-primary">Bulma CSS</span>
-              <span className="tag is-large is-primary">Node.js</span>
-              <span className="tag is-large is-primary">Express.js</span>
-              <span className="tag is-large is-primary">GIT/GitHub</span>
+              <h1 className="title is-2">Who Am I?</h1>
+              <div className="content is-4">
+                I'm a fast learner and highly motivated frontend web developer with almost two years of experience. 
+                Passionate about learning and coding, with a desire 
+                to apply my skills on larger development teams. Eager to tackle more complex 
+                projects and continue to find ways to maximize user experience.
+              </div>
 
-            </div>
+              <div className="tags">
+                <span className="tag is-large is-primary">HTML</span>
+                <span className="tag is-large is-primary">CSS</span>
+                <span className="tag is-large is-primary">JavaScript</span>
+                <span className="tag is-large is-primary">React</span>
+                <span className="tag is-large is-primary">SASS</span>
+                <span className="tag is-large is-primary">Bulma CSS</span>
+                <span className="tag is-large is-primary">Node.js</span>
+                <span className="tag is-large is-primary">Express.js</span>
+                <span className="tag is-large is-primary">GIT/GitHub</span>
+              </div>
+
             </div>
           </div>
         </div>
--- src/sections/about/Card.js
+++ src/sections/about/Card.js
@@ -5,25 +5,9 @@ const Card = () => {
   return (
     <div className="card">
     <div className="card-image">
-      <figure className="image is-150x150">
+      <figure className="image is-125x125">
         <img src={Salym} alt='Salym Akhmedov' />
       </figure>
-
-      <div className="card-content">
-        <div className="media">
-          <div className="media-content has-text-centered">
-            <h1 className="title is-2">Who Am <strong className="has-text-primary">I</strong>?</h1>
-          </div>
-        </div>
-
-        <div className="content">
-          I'm a fast learner and highly motivated frontend web developer with almost two years of experience. 
-          Passionate about learning and coding, with a desire 
-          to apply my skills on larger development teams. Eager to tackle more complex 
-          projects and continue to find ways to maximize user experience.
-        </div>
-
-      </div>
     </div>
   </div>
   )
