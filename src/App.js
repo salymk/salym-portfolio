94c71ea6d2b1064ddcec1648d90e5db24f9e7b46
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-06-29 07:38:10 -0700

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from './components/Header/Hero'
+import Navbar from './components/Header/Navbar'
+
+
 
 function App() {
   return (
     <div>
       <Hero />
+      <Navbar />
     </div>
   )
 }
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+          <a role="button" className="navbar-burger bruger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+          <a className="navbar-item">About</a>
+          <a className="navbar-item">Projects</a>
+          <a className="navbar-item">Contact</a>
+        </div>
+      </div>
+    </nav>
   )
 }
 
