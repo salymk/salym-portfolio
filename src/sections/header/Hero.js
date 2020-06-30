c8c7b4fc0ce388ea6bdb67f4caf7737bd994fcce
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-06-30 11:35:21 -0700

    Modified text for mobile

--- src/sections/header/Hero.js
+++ src/sections/header/Hero.js
@@ -5,14 +5,14 @@ const Hero = () => {
     <section className="hero is-fullheight">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title mb-0">
+          <h1 className="title is-size-3-mobile is-size-1-tablet mb-0">
             Hello, I'm <strong className="has-text-primary">Salym Akhmedov</strong>.
           </h1>
-          <h1 className="title">
+          <h1 className="title is-size-3-mobile is-size-1-tablet">
             I'm a frontend web developer.
           </h1>
 
-          <button className="button is-primary is-outlined">
+          <button className="button is-primary is-outlined is-size-5-tablet">
             <span>Find out more</span>
             <span className="icon is-small">
               <i className="fas fa-arrow-down"></i>
