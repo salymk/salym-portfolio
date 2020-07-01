b6722018889a24bd70482ee3ba393d9f5cbe637f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-01 00:00:54 -0700

    Added title to about and card

--- src/sections/main-content/about/About.js
+++ src/sections/main-content/about/About.js
@@ -8,11 +8,14 @@ const About = () => {
   return (
     <div className="section">
       <div className="container">
+        <h1 className="title has-text-centered is-1 mb-6">About</h1>
+        
         <div className="columns is-centered">
           <div className="column is-4">
             <Card />
           </div>
-          <div className="column is-6">
+          <div className="column is-6 is-mobile">
+            <h1 className="title is-3 ">Who Is This Guy?</h1>
             <p className="text-size">
               A fast learner and highly motivated frontend web developer with almost two years of experience. 
               Passionate about learning and coding, with a desire 
