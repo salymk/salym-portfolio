15c4986411b471f99f74d2f837803cf6afd2ac65
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-02 20:41:52 -0700

    Added media queries at 320 and 768

--- src/sections/main-content/about/AboutMe.js
+++ src/sections/main-content/about/AboutMe.js
@@ -6,16 +6,16 @@ const AboutMe = () => {
 		<div className='columns is-centered mt-6'>
 			<div className='column is-4'>
 				<img
-					className='image custom-img'
+					className='custom-img'
 					src={Salym2}
 					alt='Salym Akhmedov in the mountains'
 				/>
 			</div>
 			<div className='column is-6 is-mobile'>
-				<h1 className='title is-3 is-size-2-mobile is-size-2-widescreen has-text-centered-mobile'>
+				<h1 className='title mb-2 is-3 is-size-4-mobile is-size-2-widescreen has-text-centered-mobile'>
 					Who Is This Guy?
 				</h1>
-				<div className='content is-size-5-mobile is-size-6-tablet is-size-5-desktop is-size-4-widescreen'>
+				<div className='content has-text-centered-mobile is-size-6-mobile is-size-6-tablet is-size-5-desktop is-size-4-widescreen'>
 					<p className='mb-5'>
 						A fast learner and highly motivated frontend web developer with
 						almost two years of experience. Passionate about learning and
@@ -34,11 +34,3 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
-
-// <p>
-// A fast learner and highly motivated frontend web developer with almost two years of experience.
-// Passionate about learning and coding, with a desire
-// to apply his skills on larger development teams. Eager to tackle more complex
-// projects and continue to find ways to maximize user experience.
-// <a classNameName="has-text-primary"> Let's create something great together!</a>
-// </p>
