94f84793e2eab3cde2a48fbc0b155ea51a079704
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-04 08:34:00 -0700

    Delete custom media quires

--- src/sections/main-content/projects/Projects.js
+++ src/sections/main-content/projects/Projects.js
@@ -7,7 +7,6 @@ const Projects = () => {
 			<h1 className='title is-1 has-text-centered mt-6 mb-6 is-uppercase'>
 				Projects
 			</h1>
-
 			<div className='container mt-6 grid-cards grid-gap'>
 				<div className='card'>
 					<div className='card-image'>
@@ -35,19 +34,220 @@ const Projects = () => {
 							more info on the employee.
 						</div>
 
-						<button className='button is-medium mr-3 is-mobile'>
-							<span className='icon'>
-								<i className='fab fa-github'></i>
-							</span>
-							<span>GitHub</span>
-						</button>
+						<div className='buttons'>
+							<button className='button is-medium'>
+								<span className='icon'>
+									<i className='fab fa-github'></i>
+								</span>
+								<span>GitHub</span>
+							</button>
 
-						<button className='button is-medium is-mobile'>
-							<span className='icon'>
-								<i className='fab fa-github'></i>
-							</span>
-							<span>GitHub</span>
-						</button>
+							<button className='button is-medium'>
+								<span>Learn more</span>
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div className='card'>
+					<div className='card-image'>
+						<figure className='image is-4by3'>
+							<img
+								src='https://bulma.io/images/placeholders/1280x960.png'
+								alt='Placeholder image'
+							/>
+						</figure>
+					</div>
+					<div className='card-content'>
+						<div className='media'>
+							<div className='media-content'>
+								<p className='title is-3'>Employee Directory</p>
+								<p className='subtitle is-5 has-text-primary'>
+									HTML / CSS / JS
+								</p>
+							</div>
+						</div>
+
+						<div className='content'>
+							In this project, I used an API to grab employee data and displayed
+							it on a web page. You can search for the employees with their
+							names and you click on each employee and it opens a modal with
+							more info on the employee.
+						</div>
+
+						<div className='buttons'>
+							<button className='button is-medium'>
+								<span className='icon'>
+									<i className='fab fa-github'></i>
+								</span>
+								<span>GitHub</span>
+							</button>
+
+							<button className='button is-medium'>
+								<span>Learn more</span>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className='card'>
+					<div className='card-image'>
+						<figure className='image is-4by3'>
+							<img
+								src='https://bulma.io/images/placeholders/1280x960.png'
+								alt='Placeholder image'
+							/>
+						</figure>
+					</div>
+					<div className='card-content'>
+						<div className='media'>
+							<div className='media-content'>
+								<p className='title is-3'>Employee Directory</p>
+								<p className='subtitle is-5 has-text-primary'>
+									HTML / CSS / JS
+								</p>
+							</div>
+						</div>
+
+						<div className='content'>
+							In this project, I used an API to grab employee data and displayed
+							it on a web page. You can search for the employees with their
+							names and you click on each employee and it opens a modal with
+							more info on the employee.
+						</div>
+
+						<div className='buttons'>
+							<button className='button is-medium'>
+								<span className='icon'>
+									<i className='fab fa-github'></i>
+								</span>
+								<span>GitHub</span>
+							</button>
+
+							<button className='button is-medium'>
+								<span>Learn more</span>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className='card'>
+					<div className='card-image'>
+						<figure className='image is-4by3'>
+							<img
+								src='https://bulma.io/images/placeholders/1280x960.png'
+								alt='Placeholder image'
+							/>
+						</figure>
+					</div>
+					<div className='card-content'>
+						<div className='media'>
+							<div className='media-content'>
+								<p className='title is-3'>Employee Directory</p>
+								<p className='subtitle is-5 has-text-primary'>
+									HTML / CSS / JS
+								</p>
+							</div>
+						</div>
+
+						<div className='content'>
+							In this project, I used an API to grab employee data and displayed
+							it on a web page. You can search for the employees with their
+							names and you click on each employee and it opens a modal with
+							more info on the employee.
+						</div>
+
+						<div className='buttons'>
+							<button className='button is-medium'>
+								<span className='icon'>
+									<i className='fab fa-github'></i>
+								</span>
+								<span>GitHub</span>
+							</button>
+
+							<button className='button is-medium'>
+								<span>Learn more</span>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className='card'>
+					<div className='card-image'>
+						<figure className='image is-4by3'>
+							<img
+								src='https://bulma.io/images/placeholders/1280x960.png'
+								alt='Placeholder image'
+							/>
+						</figure>
+					</div>
+					<div className='card-content'>
+						<div className='media'>
+							<div className='media-content'>
+								<p className='title is-3'>Employee Directory</p>
+								<p className='subtitle is-5 has-text-primary'>
+									HTML / CSS / JS
+								</p>
+							</div>
+						</div>
+
+						<div className='content'>
+							In this project, I used an API to grab employee data and displayed
+							it on a web page. You can search for the employees with their
+							names and you click on each employee and it opens a modal with
+							more info on the employee.
+						</div>
+
+						<div className='buttons'>
+							<button className='button is-medium'>
+								<span className='icon'>
+									<i className='fab fa-github'></i>
+								</span>
+								<span>GitHub</span>
+							</button>
+
+							<button className='button is-medium'>
+								<span>Learn more</span>
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div className='card'>
+					<div className='card-image'>
+						<figure className='image is-4by3'>
+							<img
+								src='https://bulma.io/images/placeholders/1280x960.png'
+								alt='Placeholder image'
+							/>
+						</figure>
+					</div>
+					<div className='card-content'>
+						<div className='media'>
+							<div className='media-content'>
+								<p className='title is-3'>Employee Directory</p>
+								<p className='subtitle is-5 has-text-primary'>
+									HTML / CSS / JS
+								</p>
+							</div>
+						</div>
+
+						<div className='content'>
+							In this project, I used an API to grab employee data and displayed
+							it on a web page. You can search for the employees with their
+							names and you click on each employee and it opens a modal with
+							more info on the employee.
+						</div>
+
+						<div className='buttons'>
+							<button className='button is-medium'>
+								<span className='icon'>
+									<i className='fab fa-github'></i>
+								</span>
+								<span>GitHub</span>
+							</button>
+
+							<button className='button is-medium'>
+								<span>Learn more</span>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
