0f98ae16594075758fa8694f826e465187da17a6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-04 11:30:20 -0700

    Created footer

--- src/sections/main-content/footer/Footer.js
+++ src/sections/main-content/footer/Footer.js
@@ -4,7 +4,18 @@ const Footer = () => {
 	return (
 		<footer className='footer'>
 			<div className='content has-text-centered'>
-				<p>SALYM AKHMEDOV ©2020</p>
+				<span className='icon mr-4'>
+					<a href='#' className='has-text-grey-darker'>
+						<i className='fab fa-2x fa-github-square'></i>
+					</a>
+				</span>
+
+				<span className='icon'>
+					<a href='#' className='has-text-grey-darker'>
+						<i className='fab fa-2x fa-linkedin'></i>
+					</a>
+				</span>
+				<p className='mt-2'>SALYM AKHMEDOV ©2020</p>
 			</div>
 		</footer>
 	);
