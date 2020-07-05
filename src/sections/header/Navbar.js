4050c1fd7dad85bf39c1862b2fa80eee321c0a12
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-04 20:34:02 -0700

    Added smooth scroll to about item

--- src/sections/header/Navbar.js
+++ src/sections/header/Navbar.js
@@ -20,8 +20,28 @@ const Navbar = () => {
 			</div>
 			<div className='navbar-menu'>
 				<div className='navbar-start is-size-4-tablet ml-6'>
-					<a className='navbar-item'>Home</a>
-					<a className='navbar-item'>About</a>
+					<a className='navbar-item'>
+						<Link
+							activeClass='active'
+							to='home'
+							spy={true}
+							smooth={true}
+							offset={-100}
+							duration={500}>
+							Home
+						</Link>
+					</a>
+					<a className='navbar-item'>
+						<Link
+							activeClass='active'
+							to='about'
+							spy={true}
+							smooth={true}
+							offset={-100}
+							duration={500}>
+							About
+						</Link>
+					</a>
 					<a className='navbar-item'>Projects</a>
 					<a className='navbar-item'>Contact</a>
 				</div>
