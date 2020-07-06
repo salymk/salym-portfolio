36793885a92eb7172295d9243808a563c59cd959
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-06 08:07:41 -0700

    Dynamically added skill icons to tools section

--- src/sections/main-content/about/Skills.js
+++ src/sections/main-content/about/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BulmaLogo from '../../../images/bulma-logo.png';
+import icons from '../../../data/skillsData';
 
 const Skills = () => {
 	return (
@@ -9,58 +10,18 @@ const Skills = () => {
 			</h1>
 
 			<div className='container grid'>
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>HTML</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-html5 html-logo-color'></i>
-					</span>
-				</div>
-
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>CSS</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-css3-alt has-text-info	'></i>
-					</span>
-				</div>
-
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>JavaScript</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-js has-text-warning	'></i>
-					</span>
-				</div>
-
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>React</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-react has-text-link'></i>
-					</span>
-				</div>
+				{icons.map((i) => (
+					<div className='has-text-centered'>
+						<p className='subtitle is-4 mt-6'>{i.name}</p>
+						<span className='icon mt-5'>
+							<i className={i.icon}></i>
+						</span>
+					</div>
+				))}
 
 				<div className='has-text-centered'>
 					<p className='subtitle is-4 mt-6'>Bulma CSS</p>
 					<img className='bulma-logo' src={BulmaLogo} alt='Bulma CSS logo' />
-				</div>
-
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>Node.js</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-node-js has-text-success	'></i>
-					</span>
-				</div>
-
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>Git</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-git-alt git-logo-color'></i>
-					</span>
-				</div>
-
-				<div className='has-text-centered'>
-					<p className='subtitle is-4 mt-6'>GitHub</p>
-					<span className='icon mt-5'>
-						<i className='fab fa-7x fa-github github-logo-color'></i>
-					</span>
 				</div>
 			</div>
 		</div>
