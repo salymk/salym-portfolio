400f66234d8e2e1fe21d917dd8a38f75a125c33a
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-07 15:29:01 -0700

    Changed icon size to 5x

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,31 +1,31 @@
 export default [
 	{
 		name: 'HTML',
-		icon: 'fab fa-7x fa-html5 html-logo-color',
+		icon: 'fab fa-5x fa-html5 html-logo-color',
 	},
 	{
 		name: 'CSS',
-		icon: 'fab fa-7x fa-css3-alt has-text-info',
+		icon: 'fab fa-5x fa-css3-alt has-text-info',
 	},
 	{
 		name: 'JavaScript',
-		icon: 'fab fa-7x fa-js has-text-warning',
+		icon: 'fab fa-5x fa-js has-text-warning',
 	},
 	{
 		name: 'ReactJS',
-		icon: 'fab fa-7x fa-react has-text-link',
+		icon: 'fab fa-5x fa-react has-text-link',
 	},
 
 	{
 		name: 'Node.js',
-		icon: 'fab fa-7x fa-node-js has-text-succes',
+		icon: 'fab fa-5x fa-node-js has-text-succes',
 	},
 	{
 		name: 'Git',
-		icon: 'fab fa-7x fa-git-alt git-logo-color',
+		icon: 'fab fa-5x fa-git-alt git-logo-color',
 	},
 	{
 		name: 'GitHub',
-		icon: 'fab fa-7x fa-github github-logo-color',
+		icon: 'fab fa-5x fa-github github-logo-color',
 	},
 ];
