fb32f8039ee72c3409b9278599921a96328f5736
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-08 09:30:05 -0700

    Changed text size

--- src/sections/header/Hero.js
+++ src/sections/header/Hero.js
@@ -6,11 +6,11 @@ const Hero = () => {
 		<section className='hero is-fullheight has-bg-img' id='home'>
 			<div className='hero-body'>
 				<div className='container has-text-centered'>
-					<h1 className='title has-text-white is-size-4-mobile is-size-2-tablet mb-0'>
+					<h1 className='title is-5 has-text-white is-size-4-mobile is-size-2-tablet mb-0'>
 						Hello, I'm{' '}
 						<strong className='has-text-black-ter'>Salym Akhmedov</strong>.
 					</h1>
-					<h1 className='title has-text-white is-size-4-mobile is-size-2-tablet'>
+					<h1 className='title is-4 has-text-white is-size-4-mobile is-size-2-tablet'>
 						I'm a frontend web developer.
 					</h1>
 
