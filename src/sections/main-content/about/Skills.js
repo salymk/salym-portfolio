4f4631b234e585df5bb504de53119b74ae5cffd7
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-08 13:05:12 -0700

    Added styles for skills section

--- src/sections/main-content/about/Skills.js
+++ src/sections/main-content/about/Skills.js
@@ -5,21 +5,21 @@ import icons from '../../../data/skillsData';
 const Skills = () => {
 	return (
 		<div className='section has-background-primary-light'>
-			<h1 className='title is-1 has-text-centered mt-6 is-uppercase' id='tools'>
+			<h1 className='title is-2 has-text-centered mt-6 is-uppercase' id='tools'>
 				Tools
 			</h1>
 
 			<div className='container grid'>
 				{icons.map((i) => (
 					<div className='has-text-centered'>
-						<p className='subtitle is-4 mt-6'>{i.name}</p>
+						<p className='subtitle logo-title is-4 mt-6'>{i.name}</p>
 						<span className='icon mt-5'>
 							<i className={i.icon}></i>
 						</span>
 					</div>
 				))}
 
-				<div className='has-text-centered'>
+				<div className='has-text-centered my-0 mx-0'>
 					<p className='subtitle is-4 mt-6'>Bulma CSS</p>
 					<img className='bulma-logo' src={BulmaLogo} alt='Bulma CSS logo' />
 				</div>
