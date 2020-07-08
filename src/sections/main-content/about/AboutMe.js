04c21a275c5be6b42e1428fd2732d1562e1216f1
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-08 12:18:42 -0700

    Vertically centered column

--- src/sections/main-content/about/AboutMe.js
+++ src/sections/main-content/about/AboutMe.js
@@ -4,19 +4,19 @@ import Salym2 from '../../../images/mountain-salym.jpg';
 
 const AboutMe = () => {
 	return (
-		<div className='columns is-centered mt-6'>
-			<div className='column is-4'>
+		<div className='columns is-vcentered is-centered mt-6'>
+			<div className='column is-3'>
 				<img
 					className='custom-img'
 					src={Salym2}
 					alt='Salym Akhmedov in the mountains'
 				/>
 			</div>
-			<div className='column is-6 is-mobile'>
+			<div className='column ml-2 is-5 is-mobile'>
 				<h1 className='title mb-2 is-3 is-size-4-mobile has-text-centered-mobile'>
 					Who Is This Guy?
 				</h1>
-				<div className='content has-text-centered-mobile is-size-6-mobile is-size-6-tablet is-size-4-widescreen'>
+				<div className='content has-text-centered-mobile is-size-6-mobile is-size-6-tablet is-size-6-widescreen'>
 					<p className='mb-5'>
 						A fast learner and highly motivated frontend web developer with
 						almost two years of experience. Passionate about learning and
