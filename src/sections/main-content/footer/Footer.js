fb28f4e8ed4cc0008a3c2a1ac6ed4e1cb1d78b95
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-09 10:38:04 -0700

    Created footer

--- src/sections/main-content/footer/Footer.js
+++ src/sections/main-content/footer/Footer.js
@@ -3,25 +3,55 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer className='footer'>
-			<div className='content has-text-centered'>
-				<span className='icon mr-4'>
-					<a
-						href='https://github.com/salymk'
-						target='_blank'
-						className='has-text-grey-darker social-hover'>
-						<i class='fab fa-2x fa-github-square'></i>
-					</a>
-				</span>
+			<div className='container'>
+				<div className='level'>
+					<div className='level-left'>
+						<div className='level-item'>
+							<a className='title is-4' href='#'>
+								Salym Akhmedov
+							</a>
+						</div>
+					</div>
+					<div className='level-right'>
+						<a className='level-item' href='#'>
+							About
+						</a>
+						<a className='level-item' href='#'>
+							Portfolio
+						</a>
+						<a className='level-item' href='#'>
+							Tools
+						</a>
+						<a className='level-item' href='#'>
+							Contact
+						</a>
+					</div>
+				</div>
+				<hr />
+				<div className='columns'>
+					<div className='column'>
+						<div className='field is-grouped'>
+							<a
+								href='https://github.com/salymk'
+								target='_blank'
+								className='has-text-grey-darker mr-3'>
+								<i className='fab fa-2x fa-github-square'></i>
+							</a>
 
-				<span className='icon'>
-					<a
-						href='https://www.linkedin.com/in/salym-akhmedov/'
-						target='_blank'
-						className='has-text-grey-darker'>
-						<i className='fab fa-2x fa-linkedin'></i>
-					</a>
-				</span>
-				<p className='mt-2'>SALYM AKHMEDOV ©2020</p>
+							<a
+								href='https://www.linkedin.com/in/salym-akhmedov/'
+								target='_blank'
+								className='has-text-grey-darker'>
+								<i className='fab fa-2x fa-linkedin'></i>
+							</a>
+						</div>
+					</div>
+					<div className='column has-text-centered has-text-right-tablet'>
+						<p className='subtitle is-6'>
+							© 2020 Salym Akhmedov. All right reserved.
+						</p>
+					</div>
+				</div>
 			</div>
 		</footer>
 	);
