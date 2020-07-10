daf98cd7f5aa6083e32f8f4c2a9f357a00a0f7b0
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-10 07:08:14 -0700

    Removed App class and div

--- src/sections/main-content/projects/Modal.js
+++ src/sections/main-content/projects/Modal.js
@@ -7,58 +7,56 @@ const Modal = (props) => {
 	};
 
 	return (
-		<div className='App'>
-			<div className='modal'>
-				<div className='modal-background' />
-				<div className='modal-card'>
-					<header className='modal-card-head'>
-						<p className='modal-card-title'>{props.title}</p>
-						<button
-							onClick={onClick}
-							className='delete is-medium'
-							aria-label='close'
-						/>
-					</header>
+		<div className='modal'>
+			<div className='modal-background' />
+			<div className='modal-card'>
+				<header className='modal-card-head'>
+					<p className='modal-card-title'>{props.title}</p>
+					<button
+						onClick={onClick}
+						className='delete is-medium'
+						aria-label='close'
+					/>
+				</header>
 
-					<section className='modal-card-body'>
-						<iframe
-							width='360'
-							height='215'
-							src={props.youtube}
-							frameborder='0'
-							title='Project video'
-							allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-							allowfullscreen
-						/>
+				<section className='modal-card-body'>
+					<iframe
+						width='360'
+						height='215'
+						src={props.youtube}
+						frameborder='0'
+						title='Project video'
+						allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					/>
 
-						<p className='subtitle-text has-text-primary mt-3 mb-3'>
-							{props.skills}
-						</p>
-						<div className='content content-size'>{props.description}</div>
-					</section>
-					<footer className='modal-card-foot'>
-						<div className='buttons'>
-							<a
-								href={props.githubLink}
-								target='_blank'
-								className='button ct-btn'>
-								<span className='icon'>
-									<i className='fab fa-github' />
-								</span>
-								<span>GitHub</span>
-							</a>
-							<a
-								href={props.previewLink}
-								target='_blank'
-								className='button ct-btn is-warning'>
-								{props.previewLink ? 'Live Preview' : 'No Preview'}
-							</a>
-							<button onClick={onClick} className='button is-danger'>
-								Close
-							</button>
-						</div>
-					</footer>
-				</div>
+					<p className='subtitle-text has-text-primary mt-3 mb-3'>
+						{props.skills}
+					</p>
+					<div className='content content-size'>{props.description}</div>
+				</section>
+				<footer className='modal-card-foot'>
+					<div className='buttons'>
+						<a
+							href={props.githubLink}
+							target='_blank'
+							className='button ct-btn'>
+							<span className='icon'>
+								<i className='fab fa-github' />
+							</span>
+							<span>GitHub</span>
+						</a>
+						<a
+							href={props.previewLink}
+							target='_blank'
+							className='button ct-btn is-warning'>
+							{props.previewLink ? 'Live Preview' : 'No Preview'}
+						</a>
+						<button onClick={onClick} className='button is-danger'>
+							Close
+						</button>
+					</div>
+				</footer>
 			</div>
 		</div>
 	);
